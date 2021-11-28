e279f095491d978e61e186b691b3053b876272b0
hunghuy201280 <humghuy201280@gmail.com>
2021-11-28 15:06:43 +0700

    update - change api url

--- app/app/src/main/java/com/example/bankmanagement/constants/AppConfigs.kt
+++ app/app/src/main/java/com/example/bankmanagement/constants/AppConfigs.kt
@@ -2,7 +2,7 @@ package com.example.bankmanagement.constants
 
 class AppConfigs {
     companion object{
-        //const val baseUrl="https://bank-management-nodejs.vercel.app/";
-        const val baseUrl="http://10.0.2.2:3000";
+        const val baseUrl="https://uit-bank-management.herokuapp.com/";
+        //const val baseUrl="http://10.0.2.2:3000";
     }
 }
