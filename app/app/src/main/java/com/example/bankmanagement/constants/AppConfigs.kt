492cf9b8bc8daffd29c096b4f5c9834b02b5fd48
hunghuy201280 <humghuy201280@gmail.com>
2021-12-08 18:12:58 +0700

    update - change api url

--- app/app/src/main/java/com/example/bankmanagement/constants/AppConfigs.kt
+++ app/app/src/main/java/com/example/bankmanagement/constants/AppConfigs.kt
@@ -2,8 +2,8 @@ package com.example.bankmanagement.constants
 
 class AppConfigs {
     companion object{
-        //const val baseUrl="https://uit-bank-management.herokuapp.com/";
+        const val baseUrl="https://uit-bank-management.herokuapp.com/";
         //const val baseUrl="http://10.0.2.2:3000";
-        const val baseUrl="http://192.168.0.102:3000";
+        //const val baseUrl="http://192.168.0.102:3000";
     }
 }
